72fd42b51c0e1f7f478205beeefa5a8d9ecf2091
Walkman <kissgyorgy@me.com>
2013-10-07 04:06:55 +0200

    Perfect testing with nose-parameterized
    * Added test data
    * Removed all other test methods

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
-from unittest import TestCase, skip
+from nose_parameterized import parameterized
+from nose.tools import nottest, assert_equal
+from test_data import HAND1, HAND2
+import unittest
 import pytz
 from decimal import Decimal
 from datetime import datetime
@@ -7,131 +10,138 @@ from handparser import PokerStarsHand
 
 
 ET = pytz.timezone('US/Eastern')
-HAND1 = file('hand1.txt').read()
 
 
-class TestParseHeader1(TestCase):
+class BaseHeaderTest(unittest.TestCase):
     def setUp(self):
-        self.hand = PokerStarsHand(HAND1, parse=False)
+        self.hand = PokerStarsHand(self.hh, parse=False)
         self.hand.parse_header()
 
     def tearDown(self):
         del self.hand
 
-    def test_flag_set(self):
-        self.assertTrue(self.hand.header_parsed)
-        self.assertFalse(self.hand.parsed)
 
-    def test_poker_room(self):
-        self.assertEqual(self.hand.room, "STARS")
-
-    def test_hand_number(self):
-        self.assertEqual(self.hand.number, "105024000105")
-
-    def test_tour(self):
-        self.assertEqual(self.hand.type, "TOUR")
-        self.assertEqual(self.hand.tour_number, "797469411")
-        self.assertEqual(self.hand.tour_level, "I")
-
-    def test_currency(self):
-        self.assertEqual(self.hand.currency, "USD")
-
-    def test_stake(self):
-        self.assertEqual(self.hand.buyin, Decimal('3.19'))
-        self.assertEqual(self.hand.rake, Decimal('0.31'))
-
-    def test_game(self):
-        self.assertEqual(self.hand.game, "HOLDEM")
-
-    def test_limit(self):
-        self.assertEqual(self.hand.limit, "NL")
-
-    def test_blinds(self):
-        self.assertEqual(self.hand.sb, Decimal(10))
-        self.assertEqual(self.hand.bb, Decimal(20))
-
-    def test_date(self):
-        self.assertEqual(self.hand.date, ET.localize(datetime(2013, 10, 4, 13, 53, 27)))
-
-
-class TestParseBody1(TestCase):
+class BaseBodyTest(unittest.TestCase):
     def setUp(self):
-        self.hand = PokerStarsHand(HAND1)
+        self.hand = PokerStarsHand(self.hh)
 
     def tearDown(self):
         del self.hand
 
-    def test_parsed_flags_are_set(self):
-        self.assertTrue(self.hand.parsed)
-        self.assertTrue(self.hand.header_parsed)
 
-    def test_table_name(self):
-        self.assertEqual('797469411 15', self.hand.table_name)
+class TestHeaderHand1(BaseHeaderTest):
+    hh = HAND1
 
-    def test_max_players(self):
-        self.assertEqual(9, self.hand.max_players)
+    @parameterized.expand([
+        ('room', 'STARS'),
+        ('number', '105024000105'),
+        ('type', 'TOUR'),
+        ('tour_number', '797469411'),
+        ('tour_level', 'I'),
+        ('currency', 'USD'),
+        ('buyin', Decimal('3.19')),
+        ('rake', Decimal('0.31')),
+        ('game', 'HOLDEM'),
+        ('limit', 'NL'),
+        ('sb', Decimal(10)),
+        ('bb', Decimal(20)),
+        ('date', ET.localize(datetime(2013, 10, 4, 13, 53, 27)))
+    ])
+    def test_header(self, attr, res):
+        assert getattr(self.hand, attr) == res
 
-    def test_button_seat(self):
-        self.assertEqual(1, self.hand.button_seat)
 
-    def test_button(self):
-        self.assertEqual('flettl2', self.hand.button)
+class TestBodyHand1(BaseBodyTest):
+    hh = HAND1
 
-    def test_hero(self):
-        self.assertEqual('W2lkm2n', self.hand.hero)
+    @parameterized.expand([
+        ('table_name', '797469411 15'),
+        ('max_players', 9),
+        ('button_seat', 1),
+        ('button', 'flettl2'),
+        ('hero', 'W2lkm2n'),
+        ('hero_seat', 5),
+        ('players', OrderedDict([('flettl2', 1500), ('santy312', 3000), ('flavio766', 3000),
+                                 ('strongi82', 3000), ('W2lkm2n', 3000), ('MISTRPerfect', 3000),
+                                 ('blak_douglas', 3000), ('sinus91', 1500), ('STBIJUJA', 1500)])),
+        ('hero_hole_cards', ('Ac', 'Jh')),
+        ('flop', ('2s', '6d', '6h')),
+        ('turn', None),
+        ('river', None),
+        ('board', ('2s', '6d', '6h')),
+        ('preflop_actions', ("strongi82, folds",
+                             "W2lkm2n, raises 40 to 60",
+                             "MISTRPerfect, calls 60",
+                             "blak_douglas, folds",
+                             "sinus91, folds",
+                             "STBIJUJA, folds",
+                             "flettl2, folds",
+                             "santy312, folds",
+                             "flavio766, folds")),
+        ('flop_actions', ('W2lkm2n, bets 80', 'MISTRPerfect, folds', 'Uncalled bet (80) returned to W2lkm2n',
+                          'W2lkm2n collected 150 from pot', "W2lkm2n, doesn't show hand")),
+        ('turn_actions', None),
+        ('river_actions', None),
+        ('total_pot', Decimal(150)),
+        ('winners', ('W2lkm2n',)),
+    ])
+    def test_body(self, attr, res):
+        assert getattr(self.hand, attr) == res
 
-    def test_hero_seat(self):
-        self.assertEqual(5, self.hand.hero_seat)
 
-    def test_players_and_stacks(self):
-        self.assertEqual(self.hand.players, OrderedDict([('flettl2', 1500), ('santy312', 3000), ('flavio766', 3000),
-                                                         ('strongi82', 3000), ('W2lkm2n', 3000), ('MISTRPerfect', 3000),
-                                                         ('blak_douglas', 3000), ('sinus91', 1500), ('STBIJUJA', 1500)])
-                        )
+class TestHeaderHand2(BaseHeaderTest):
+    hh = HAND2
 
-    def test_hero(self):
-        self.assertEqual('W2lkm2n', self.hand.hero)
+    @parameterized.expand([('room', 'STARS'),
+                           ('number', '105034215446'),
+                           ('type', 'TOUR'),
+                           ('tour_number', '797536898'),
+                           ('tour_level', 'XI'),
+                           ('currency', 'USD'),
+                           ('buyin', Decimal('3.19')),
+                           ('rake', Decimal('0.31')),
+                           ('game', 'HOLDEM'),
+                           ('limit', 'NL'),
+                           ('sb', Decimal(400)),
+                           ('bb', Decimal(800)),
+                           ('date', ET.localize(datetime(2013, 10, 4, 17, 22, 20))),
+    ])
+    def test_header(self, attr, res):
+        assert_equal(getattr(self.hand, attr), res)
 
-    def test_hero_hole_cards(self):
-        self.assertEqual(('Ac', 'Jh'), self.hand.hero_hole_cards)
 
-    def test_flop(self):
-        self.assertEqual(('2s', '6d', '6h'), self.hand.flop)
+class TestBodyHand2(BaseBodyTest):
+    hh = HAND2
 
-    def test_turn(self):
-        self.assertIsNone(self.hand.turn)
-
-    def test_river(self):
-        self.assertIsNone(self.hand.river)
-
-    def test_board(self):
-        self.assertEqual(('2s', '6d', '6h'), self.hand.board)
-
-    def test_winners(self):
-        self.assertEqual(('W2lkm2n',), self.hand.winners)
-
-    def test_total_pot(self):
-        self.assertEqual(Decimal(150), self.hand.total_pot)
-
-    def test_preflop_actions(self):
-        self.assertEqual(("strongi82: folds",
-                          "W2lkm2n: raises 40 to 60",
-                          "MISTRPerfect: calls 60",
-                          "blak_douglas: folds",
-                          "sinus91: folds",
-                          "STBIJUJA: folds",
-                          "flettl2: folds",
-                          "santy312: folds",
-                          "flavio766: folds"),
-                         self.hand.preflop_actions)
-
-    def test_flop_actions(self):
-        self.assertEqual(('W2lkm2n: bets 80', 'MISTRPerfect: folds', 'Uncalled bet (80) returned to W2lkm2n',
-                          'W2lkm2n collected 150 from pot', "W2lkm2n: doesn't show hand"),
-                         self.hand.flop_actions)
-
-    def test_turn_actions(self):
-        self.assertIsNone(self.hand.turn_actions)
-
-    def test_river_actions(self):
-        self.assertIsNone(self.hand.river_actions)
+    @parameterized.expand([('table_name', '797536898 9'),
+                           ('max_players', 9),
+                           ('button_seat', 2),
+                           ('button', 'W2lkm2n'),
+                           ('hero', 'W2lkm2n'),
+                           ('hero_seat', 2),
+                           ('players', OrderedDict([('RichFatWhale', 12910), ('W2lkm2n', 11815), ('Labahra', 7395),
+                                                    ('Lean Abadia', 7765), ('lkenny44', 10080), ('Newfie_187', 1030),
+                                                    ('Hokolix', 13175), ('pmmr', 2415), ('costamar', 13070)])),
+                           ('hero_hole_cards', ('Jd', 'Js')),
+                           ('flop', ('3c', '6s', '9d')),
+                           ('turn', '8d'),
+                           ('river', 'Ks'),
+                           ('board', ('2s', '6d', '6h')),
+                           ('preflop_actions', ("lkenny44, folds",
+                                                "Newfie_187, raises 155 to 955 and is all-in",
+                                                "Hokolix, folds",
+                                                "pmmr, folds",
+                                                "costamar, raises 12040 to 12995 and is all-in"
+                                                "RichFatWhale, folds"
+                                                "W2lkm2n, calls 11740 and is all-in"
+                                                "Labahra, folds"
+                                                "Lean Abadia, folds"
+                                                "Uncalled bet (1255) returned to costamar")),
+                           ('flop_actions', None),
+                           ('turn_actions', None),
+                           ('river_actions', None),
+                           ('total_pot', Decimal(26310)),
+                           ('winners', ('costamar',)),
+    ])
+    def test_body(self, attr, res):
+        assert getattr(self.hand, attr) == res
