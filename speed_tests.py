f44423418beb595744cf62d9c0c8823d205926b3
Walkman <kissgyorgy@me.com>
2013-10-10 08:13:40 +0200

    speed test and results

--- speed_tests.py
+++ speed_tests.py
@@ -1,15 +1,18 @@
 from timeit import timeit, repeat
 
 
+results, single_results = [], []
 for handnr in range(1, 5):
-    print "HAND%s Single run:" % handnr,
-    print timeit('PokerStarsHand(HAND%s)' % handnr,
-                 number=10000,
-                 setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr)
+    #print "HAND%s Single run:" % handnr,
+    single_results.append(timeit('PokerStarsHand(HAND%s)' % handnr,
+                   number=100000,
+                   setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr))
 
-    print "HAND%s repeated run:" % handnr,
-    print repeat('PokerStarsHand(HAND%s)' % handnr,
-                 repeat=5,
-                 number=10000,
-                 setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr)
+    #print "HAND%s repeated run:" % handnr,
+    results.extend(repeat('PokerStarsHand(HAND%s)' % handnr,
+                   repeat=3,
+                   number=100000,
+                   setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr))
 
+print "Single results average: %s" % (sum(single_results) / len(single_results))
+print "Repeated results average: %s" % (sum(results) / len(results))
