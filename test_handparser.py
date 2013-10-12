76e63a891a4856dbd7d445c74efe6e9703779177
Walkman <kissgyorgy@me.com>
2013-10-12 08:07:15 +0200

    Add conftest.py for fixtures

--- test_handparser.py
+++ test_handparser.py
@@ -6,28 +6,6 @@ from decimal import Decimal
 from datetime import datetime
 
 
-all_test_hands = [eval('hand_data.HAND%d' % num) for num in range(1, 6)]
-
-@pytest.fixture(params=all_test_hands)
-def all_hands(request):
-    """Parse all hands from test_data and returns a PokerStarsHand instance."""
-    return PokerStarsHand(request.param)
-
-
-@pytest.fixture
-def hand(request):
-    """Parse handhistory defined in hand_text class attribute and returns a PokerStarsHand instance."""
-    return PokerStarsHand(request.instance.hand_text)
-
-
-@pytest.fixture
-def hand_header(request):
-    """Parse hand history header only defined in hand_text and returns a PokerStarsHand instance."""
-    h = PokerStarsHand(request.instance.hand_text, parse=False)
-    h.parse_header()
-    return h
-
-
 class TestDictBehavior:
     def test_hand_is_mutablemapping(self, all_hands):
         assert isinstance(all_hands, MutableMapping)
