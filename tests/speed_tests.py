b356e2e1a3a6cd91efe8fd36a3e52eeb3e000a70
Walkman <kissgyorgy@me.com>
2013-10-15 20:29:55 +0200

    Speed test fix

--- tests/speed_tests.py
+++ tests/speed_tests.py
@@ -6,13 +6,13 @@ for handnr in range(1, 5):
     #print "HAND%s Single run:" % handnr,
     single_results.append(timeit('PokerStarsHand(HAND%s)' % handnr,
                    number=100000,
-                   setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr))
+                   setup="from handparser import PokerStarsHand; from hand_data import HAND%s" % handnr))
 
     #print "HAND%s repeated run:" % handnr,
     results.extend(repeat('PokerStarsHand(HAND%s)' % handnr,
                    repeat=3,
                    number=100000,
-                   setup="from handparser import PokerStarsHand; from test_data import HAND%s" % handnr))
+                   setup="from handparser import PokerStarsHand; from hand_data import HAND%s" % handnr))
 
 print "Single results average: %s" % (sum(single_results) / len(single_results))
 print "Repeated results average: %s" % (sum(results) / len(results))
