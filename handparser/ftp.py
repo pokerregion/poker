196d8217e8c783cfa9eba0fb9ee19b919c98c340
Walkman <kissgyorgy@me.com>
2013-10-16 09:44:17 +0200

    Consistent date format accross different sites
    The date regexp should find the whole date string between [] and date format
    should convert it to the right datetime object.

--- handparser/ftp.py
+++ handparser/ftp.py
@@ -1,1 +1,12 @@
+from handparser.common import PokerHand
 
+
+class FullTiltHand(PokerHand):
+    """Parses Full Tilt Poker hands.
+
+    Class specific attributes:
+        poker_room      -- FTP
+
+    """
+    poker_room = 'FTP'
+    date_format = '%H:%M:%S ET - %Y/%m/%d'
