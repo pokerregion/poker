acf7f9ab6f571d8d5a89e163ae9b4daa7f572cf7
Kiss György <kissgyorgy@me.com>
2014-08-23 15:13:02 +0200

    Three different implementation of the Hand metaclass.
    _HandMeta2 is best, because it stores all the Hand instances on the class
    itself at class creation time, so the first call will be very fast.
    _HandMeta is good, but every iteration the Hands would be generated again.
    _HandMeta3 is only fast after the first call.

--- poker/hand.py
+++ poker/hand.py
@@ -3,7 +3,7 @@ import random
 import itertools
 import functools
 from decimal import Decimal
-from functools import total_ordering
+from functools import total_ordering, lru_cache
 from cached_property import cached_property
 from ._common import _MultiValueEnum, _ReprMixin
 from .card import Suit, Rank, Card, BROADWAY_RANKS
@@ -15,14 +15,70 @@ class Shape(_MultiValueEnum):
     PAIR =  '',
 
 
+class _HandMeta(type):
+    """Makes Hand class iterable. __iter__ goes through all hands in ascending order."""
+    def _get_non_pairs(cls):
+        for rank1 in Rank:
+            for rank2 in (r for r in Rank if r < rank1):
+                yield cls(rank1.value + rank2.value + 'o')
+                yield cls(rank1.value + rank2.value + 's')
+
+    def _get_pairs(cls):
+        for rank in Rank:
+            yield cls(rank.value * 2)
+
+    def __iter__(cls):
+        yield from cls._get_non_pairs()
+        yield from cls._get_pairs()
+
+
+class _HandMeta2(type):
+    """Makes Hand class iterable. __iter__ goes through all hands in ascending order."""
+    def __new__(metacls, clsname, bases, classdict):
+        cls = super().__new__(metacls, clsname, bases, classdict)
+        cls._all_hands = list(cls._get_non_pairs()) + list(cls._get_pairs())
+        return cls
+
+    def _get_non_pairs(cls):
+        for rank1 in Rank:
+            for rank2 in (r for r in Rank if r < rank1):
+                yield cls(rank1.value + rank2.value + 'o')
+                yield cls(rank1.value + rank2.value + 's')
+
+    def _get_pairs(cls):
+        for rank in Rank:
+            yield cls(rank.value * 2)
+
+    def __iter__(cls):
+        return iter(cls._all_hands)
+
+
+class _HandMeta3(type):
+    """Makes Hand class iterable. __iter__ goes through all hands in ascending order."""
+    def _get_non_pairs(cls):
+        for rank1 in Rank:
+            for rank2 in (r for r in Rank if r < rank1):
+                yield cls(rank1.value + rank2.value + 'o')
+                yield cls(rank1.value + rank2.value + 's')
+
+    def _get_pairs(cls):
+        for rank in Rank:
+            yield cls(rank.value * 2)
+
+    @lru_cache(maxsize=2)
+    def __iter__(cls):
+        yield from cls._get_non_pairs()
+        yield from cls._get_pairs()
+
+
 @total_ordering
-class Hand(_ReprMixin):
+class Hand(_ReprMixin, metaclass=_HandMeta2):
     """General hand without a precise suit. Only knows about two ranks and shape."""
 
     __slots__ = ('_first', '_second', '_shape')
 
     def __new__(cls, hand):
-        if isinstance(hand, Hand):
+        if isinstance(hand, cls):
             return hand
 
         if len(hand) not in (2, 3):
@@ -30,7 +86,7 @@ class Hand(_ReprMixin):
 
         first, second = hand[:2]
 
-        self = super().__new__(cls)
+        self = object.__new__(cls)
 
         if len(hand) == 2:
             if first != second:
@@ -164,15 +220,13 @@ class Hand(_ReprMixin):
         self._shape = Shape(value)
 
 
-PAIR_HANDS = tuple(Hand(rank.value * 2) for rank in list(Rank))
+PAIR_HANDS = tuple(hand for hand in Hand if hand.is_pair)
 """Tuple of all pair hands in ascending order."""
 
-OFFSUIT_HANDS = tuple(Hand(hand1.value + hand2.value + 'o') for hand1, hand2 in
-                      itertools.combinations(list(Rank), 2))
+OFFSUIT_HANDS = tuple(hand for hand in Hand if hand.is_offsuit)
 """Tuple of offsuit hands in ascending order."""
 
-SUITED_HANDS = tuple(Hand(hand1.value + hand2.value + 's') for hand1, hand2 in
-                     itertools.combinations(list(Rank), 2))
+SUITED_HANDS = tuple(hand for hand in Hand if hand.is_suited)
 """Tuple of suited hands in ascending order."""
 
 
