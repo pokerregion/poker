8e3b712146cb093990d168a8430f6ea2b1bb0951
Kiss György <kissgyorgy@me.com>
2014-08-23 18:03:17 +0200

    Automatically get version from package metadata.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(TestCommand):
 
 setup(
     name = 'poker',
-    version = '0.11.0',
+    version = '0.12.0',
     description = 'Poker Framework',
     classifiers = [
         "Intended Audience :: Developers",
