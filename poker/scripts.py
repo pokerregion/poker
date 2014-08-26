6e53b45412f42a2cbfc0b13d0664df65df01e187
Kiss György <kissgyorgy@me.com>
2014-08-26 15:03:27 +0200

    Shell handles redirecting to files just fine, no need to save.

--- poker/scripts.py
+++ poker/scripts.py
@@ -11,28 +11,14 @@ def poker():
 @click.argument('range')
 @click.option('--no-border', is_flag=True, help="Don't show border.")
 @click.option('--html', is_flag=True, help="Output html, so you can paste it on a website.")
-@click.option('--outfile', type=click.Path(writable=True), help="Save the result to a file.")
-def range(range, no_border, html, outfile):
+def range(range, no_border, html):
     """Prints the given range in a formatted table either in a plain ASCII or HTML.
     The only required argument is the range definition, e.g. "A2s+ A5o+ 55+"
     """
     from .hand import Range
     border = not no_border
-
     result = Range(range).as_html() if html else Range(range).as_table(border)
-
-    if not outfile:
-        click.echo(result)
-        return
-
-    if Path(outfile).exists():
-        confirmed = click.confirm(
-            'The file "{}" already exists. Are you sure you want to overwrite?'
-            .format(outfile), abort=True
-        )
-    with open(outfile, 'w') as fp:
-        fp.write(result)
-    click.echo('File "{}" saved.'.format(outfile))
+    click.echo(result)
 
 
 @poker.command()
