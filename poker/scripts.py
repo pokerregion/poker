d651f8fcf98c464c122ab4a29066deed697102f5
Kiss György <kissgyorgy@me.com>
2014-08-26 04:01:46 +0200

    Two plus two info command.

--- poker/scripts.py
+++ poker/scripts.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import click
-from .hand import Range
 
 
 @click.group()
@@ -17,6 +16,7 @@ def range(range, no_border, html, outfile):
     """Prints the given range in a formatted table either in a plain ASCII or HTML.
     The only required argument is the range definition, e.g. "A2s+ A5o+ 55+"
     """
+    from .hand import Range
     border = not no_border
 
     result = Range(range).as_html() if html else Range(range).as_table(border)
@@ -33,3 +33,31 @@ def range(range, no_border, html, outfile):
     with open(outfile, 'w') as fp:
         fp.write(result)
     click.echo('File "{}" saved.'.format(outfile))
+
+
+@poker.command()
+@click.argument('id')
+def get2p2info(id):
+    """Get profile information about a Two plus Two member given the id."""
+    from .website.twoplustwo import TwoPlusTwoForumMember
+    from dateutil.tz import tzlocal
+    localtimezone = tzlocal()
+
+    member = TwoPlusTwoForumMember(id)
+    info = (
+        ('Forum id', member.id),
+        ('Username', member.username),
+        ('Location', member.location),
+        ('Total posts', member.total_posts),
+        ('Posts per day', member.posts_per_day),
+        ('Rank', member.rank),
+        ('Last activity', member.last_activity.astimezone(localtimezone)
+                                .strftime('%Y-%m-%d (%A) %H:%I:%S (%Z)')),
+        ('Join date', member.join_date.strftime('%Y-%m-%d')),
+        ('Usergroups', member.public_usergroups),
+        ('Profile picture', member.profile_picture),
+    )
+    for what, value in info:
+        if not value:
+            value = '-'
+        click.echo((what + ': ').ljust(20) + str(value))
