f63fb0f7bcff03183e1d584934ec08ddcd0606bb
Kiss György <kissgyorgy@me.com>
2014-08-28 18:24:52 +0200

    get_status() for pokerstars

--- poker/website/pokerstars.py
+++ poker/website/pokerstars.py
@@ -41,3 +41,40 @@ def get_current_tournaments():
         )
 
 
+_PokerStarsStatus = namedtuple('PokerStarsStatus',
+    'updated '
+    'tables '
+    'next_update '
+    'players '
+    'clubs '
+    'active_tournaments '
+    'total_tournaments '
+    'sites '                # list of sites, including Play Money
+    'club_members '
+)
+
+
+_PokerStarsSiteStatus = namedtuple('PokerStarsSiteStatus',
+    'id '       # ".FR", ".ES", ".IT" or 'Play Money'
+    'tables '
+    'players '
+    'active_tournaments '
+)
+
+
+def get_status():
+    """Get pokerstars status: players online, number of tables, etc."""
+
+    res = requests.get(STATUS_URL)
+    status = res.json()['tournaments']['summary']
+
+    # move all sites under sites attribute, including play money
+    sites = status.pop('site')
+    play_money = status.pop('play_money')
+    play_money['id'] = 'Play Money'
+    sites.append(play_money)
+    sites = tuple(_PokerStarsSiteStatus(**site) for site in sites)
+
+    updated = parse_date(status.pop('updated'))
+
+    return _PokerStarsStatus(sites=sites, updated=updated, **status)
