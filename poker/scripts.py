41548d3937703dd13a7fb4175602055e1d44f436
Kiss György <kissgyorgy@me.com>
2014-08-29 05:49:59 +0200

    common print function which formats types
    You only need to pass values and it will automatically find out how
    to display it in a consistent way accross all subcommands.
    Datetime objects are represented in the local timezone.

--- poker/scripts.py
+++ poker/scripts.py
@@ -1,18 +1,41 @@
 import sys
 from pathlib import Path
+from collections.abc import Sequence
 from contextlib import contextmanager
+from datetime import datetime, date
+from dateutil.tz import tzlocal
 import click
+
+
+LOCALTIMEZONE = tzlocal()
 
 
 # FIXME: This is a hack about click incapability to prompt to stderr.
 # See: https://github.com/mitsuhiko/click/issues/211
 @contextmanager
-def redirect_stdout_to_stderr():
+def _redirect_stdout_to_stderr():
     """Redirect standard output to standard error and restore after the context is finished."""
     oldout = sys.stdout
     sys.stdout = sys.stderr
     yield
     sys.stdout = oldout
+
+
+def _print_values(info):
+    for what, value in info:
+        valueformat = "{!s}"
+        if not value:
+            value = '-'
+        elif isinstance(value, int):
+            valueformat = "{:,}"
+        elif isinstance(value, datetime):
+            value = value.astimezone(LOCALTIMEZONE)
+            valueformat = "{:%Y-%m-%d (%A) %H:%M (%Z)}"
+        elif isinstance(value, date):
+            valueformat = "{:%Y-%m-%d}"
+        elif isinstance(value, Sequence) and not isinstance(value, str):
+            value = ', '.join(value)
+        click.echo(('{:<20}' + valueformat).format(what + ': ', value))
 
 
 @click.group()
@@ -30,6 +53,7 @@ def range_(range, no_border, html):
     The only required argument is the range definition, e.g. "A2s+ A5o+ 55+"
     """
     from .hand import Range
+
     border = not no_border
     result = Range(range).as_html() if html else Range(range).as_table(border)
     click.echo(result)
@@ -43,8 +67,6 @@ def twoplustwo(username):
     from .website.twoplustwo import (
         TwoPlusTwoForumMember, AmbiguousUserNameError, UserNotFoundError
     )
-    from dateutil.tz import tzlocal
-    localtimezone = tzlocal()
 
     try:
         member = TwoPlusTwoForumMember(username)
@@ -56,7 +78,7 @@ def twoplustwo(username):
         for ind, user in enumerate(e.users):
             click.echo('{}. {}'.format(ind + 1, user.name), err=True)
 
-        with redirect_stdout_to_stderr():
+        with _redirect_stdout_to_stderr():
             number = click.prompt('Which would you like to see [{}-{}]'.format(1, len(e.users)),
                                   prompt_suffix='? ', type=click.IntRange(1, len(e.users)))
 
@@ -68,12 +90,6 @@ def twoplustwo(username):
     click.echo('Two plus two forum member')
     click.echo('-------------------------')
 
-    if member.last_activity:
-        last_activity = member.last_activity.astimezone(localtimezone)\
-                                            .strftime('%Y-%m-%d (%A) %H:%M:%S (%Z)')
-    else:
-        last_activity = None
-
     info = (
         ('Username', member.username),
         ('Forum id', member.id),
@@ -81,24 +97,21 @@ def twoplustwo(username):
         ('Total posts', member.total_posts),
         ('Posts per day', member.posts_per_day),
         ('Rank', member.rank),
-        ('Last activity', last_activity),
-        ('Join date', member.join_date.strftime('%Y-%m-%d')),
-        ('Usergroups', ', '.join(member.public_usergroups)),
+        ('Last activity', member.last_activity),
+        ('Join date', member.join_date),
+        ('Usergroups', member.public_usergroups),
         ('Profile picture', member.profile_picture),
         ('Avatar', member.avatar),
     )
 
-    for what, value in info:
-        if not value:
-            value = '-'
-        click.echo('{:<20}{!s}'.format(what + ': ', value))
+    _print_values(info)
 
 
-@poker.command(short_help="List pocketfives ranked players.")
+@poker.command(short_help="List pocketfives ranked players (1-100).")
 @click.argument('num', type=click.IntRange(1, 100), default=100)
-def p5_players(num):
-    """List pocketfives ranked players, max 100 if no num, or num if specified."""
-    from poker.website.pocketfives import get_ranked_players
+def p5players(num):
+    """List pocketfives ranked players, max 100 if no NUM, or NUM if specified."""
+    from .website.pocketfives import get_ranked_players
 
     format_str = '{:>4.4}   {!s:<15.13}{!s:<18.15}{!s:<9.6}{!s:<10.7}'\
                  '{!s:<14.11}{!s:<12.9}{!s:<12.9}{!s:<12.9}{!s:<4.4}'
@@ -114,3 +127,31 @@ def p5_players(num):
         click.echo(format_str.format(str(ind + 1) + '.', *player))
         if ind == num - 1:
             break
+
+
+@poker.command(short_help="Show PokerStars status like active players.")
+def psstatus():
+    """Shows PokerStars status such as number of players, tournaments."""
+    from .website.pokerstars import get_status
+
+    click.echo('PokerStars status')
+    click.echo('-----------------')
+    status = get_status()
+    status_info = (
+        ('Info updated', status.updated),
+        ('Tables', status.tables),
+        ('Players', status.players),
+        ('Active tournaments', status.active_tournaments),
+        ('Total tournaments', status.total_tournaments),
+        ('Clubs', status.clubs),
+        ('Club members', status.club_members),
+    )
+
+    _print_values(status_info)
+
+    site_format_str = '{0.id:<12}  {0.tables:<7}  {0.players:<8}  {0.active_tournaments}'
+    click.echo('\nSite          Tables   Players   Tournaments')
+    click.echo('-----------   ------   -------   -----------')
+
+    for site in status.sites:
+        click.echo(site_format_str.format(site))
