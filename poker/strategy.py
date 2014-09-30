e100427060aff93a3312694c44e77721233c1e7f
Kiss György <kissgyorgy@me.com>
2014-09-30 16:22:31 +0200

    strategy files are encoded with UTF-8. Need to read text from them.

--- poker/strategy.py
+++ poker/strategy.py
@@ -15,9 +15,9 @@ _POSITIONS = {'utg', 'utg1', 'utg2', 'utg3', 'utg4', 'co', 'btn', 'sb', 'bb'}
 
 
 class Strategy(Mapping):
-    def __init__(self, strategy):
+    def __init__(self, strategy, source='<string>'):
         self._config = ConfigParser(default_section='strategy', interpolation=None)
-        self._config.read_string(strategy)
+        self._config.read_string(strategy, source)
 
         self._situations = odict()
         for name in self._config.sections():
@@ -35,6 +35,12 @@ class Strategy(Mapping):
             self._situations[name] = _Situation(**values)
 
         self._tuple = tuple(self._situations.values())
+
+    @classmethod
+    def from_file(cls, filename):
+        # Path accept str or Path
+        strategy = Path(filename).open(encoding='utf-8').read()
+        return cls(strategy, source=filename)
 
     def __getattr__(self, name):
         # Strategy uses only _Situation._fields, but this way .strategy files are more flexible,
@@ -68,12 +74,6 @@ class Strategy(Mapping):
 
     def __len__(self):
         return len(self._situations)
-
-    @classmethod
-    def from_file(cls, filename):
-        # Path accept str or Path
-        strategy = Path(filename).open().read()
-        return cls(strategy)
 
     def get_first_spot(self, situation=0):
         situation = self[situation]
