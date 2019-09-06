c5e47e61a6b51da99126a9faa4064a621acf017c
Kiss György <kissgyorgy@me.com>
2019-09-06 20:35:50 +0200

    Make handhistory speed test work from root dir

--- tests/handhistory/speed_tests.py
+++ tests/handhistory/speed_tests.py
@@ -5,12 +5,12 @@ results, single_results = [], []
 for handnr in range(1, 5):
     single_results.append(
         timeit(f'PokerStarsHandHistory(HAND{handnr})', number=100000,
-               setup="from handhistory import PokerStarsHandHistory; "
-                     f"from stars_hands import HAND{handnr}")
+               setup="from poker.room.pokerstars import PokerStarsHandHistory; "
+                     f"from tests.handhistory.stars_hands import HAND{handnr}")
     )
     results.extend(repeat(f'PokerStarsHandHistory(HAND{handnr})', repeat=3, number=100000,
-                   setup="from handhistory import PokerStarsHandHistory; "
-                         f"from stars_hands import HAND{handnr}")
+                   setup="from poker.room.pokerstars import PokerStarsHandHistory; "
+                         f"from tests.handhistory.stars_hands import HAND{handnr}")
     )
 
 print("Single results average:", sum(single_results) / len(single_results))
