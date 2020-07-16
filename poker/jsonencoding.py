e2b813c542c95d5dfc85b0b64888ccacbe734f6e
Chris060986 <christoph.birk@googlemail.com>
2020-07-16 19:42:46 +0200

    Add test cases for street and board, start handler for HH

--- poker/jsonencoding.py
+++ poker/jsonencoding.py
@@ -2,7 +2,7 @@ import jsonpickle
 from jsonpickle.handlers import BaseHandler
 
 from poker import Card
-from poker.handhistory import _BaseStreet
+from poker.handhistory import _BaseStreet, _BaseHandHistory
 
 
 #TODO: Create test case!!
@@ -20,25 +20,39 @@ class CardHandler(BaseHandler):
     def restore(self, obj):
         raise NotImplementedError
 
+
 @jsonpickle.handlers.register(_BaseStreet, base=True)
 class StreetHandler(BaseHandler):
 
     def flatten(self, obj, data):
         data = {}
+        if obj.actions is not None:
+            data['actions'] = [self.context.flatten(action, reset=False) for action in obj.actions]
         if obj.cards is not None:
-            cards = [self.context.flatten(x, reset=False) for x in obj.cards]
-            data['cards'] = cards
+            data['cards'] = [self.context.flatten(x, reset=False) for x in obj.cards]
         return data
 
     def restore(self, obj):
         raise NotImplementedError
 
+@jsonpickle.handlers.register(_BaseHandHistory, base=True)
+class HandHistoryHandler(BaseHandler):
+
+    def flatten(self, obj, data):
+        data = {}
+        # TODO: implement all the toplevel objects of handhistory
+        data['board'] = [self.context.flatten(x, reset=False) for x in obj.board]
+        return data
+
+    def restore(self, obj):
+        raise NotImplementedError
 
 class JsonEncoder:
 
     def __init__(self):
         jsonpickle.handlers.register(CardHandler)
         jsonpickle.handlers.register(StreetHandler)
+        jsonpickle.handlers.register(HandHistoryHandler)
 
     def encode(self, obj):
         return jsonpickle.encode(obj)
--- tests/test_jsonencoding.py
+++ tests/test_jsonencoding.py
@@ -1,11 +1,60 @@
 import pytest
+from poker.room.pokerstars import _Street, PokerStarsHandHistory
+
+from poker.handhistory import _BaseStreet
+
 from poker.card import Card
 from poker.jsonencoding import JsonEncoder
+from tests.handhistory import stars_hands
+
+
+@pytest.fixture(scope='function')
+def json_encoder():
+    return JsonEncoder()
 
 
 class TestCardEncoding:
 
-    def tests_simpleCard(self):
-        encoder = JsonEncoder()
+    def tests_simple_card(self, json_encoder):
         card = Card("Ad")
-        assert encoder.encode(card) == "{\"rank\": \"A\", \"suit\": \"DIAMONDS\"}"
+        assert json_encoder.encode(card) == "{\"rank\": \"A\", \"suit\": \"DIAMONDS\"}"
+
+    def test_board_encoding(self, json_encoder):
+        # TODO
+        raise
+
+class TestHeaderEncoding:
+    # TODO: date, gametype etc.
+    def test_test(self, json_encoder):
+        raise
+
+class TestStreetEncoding:
+    # TODO: date, gametype etc.
+    def test_street_flop_encoding(self, json_encoder):
+        street = _Street(["[Ad Ks Qc]",],)
+        assert json_encoder.encode(street) == """{"cards": [{"rank": "A", "suit": "DIAMONDS"}, {"rank": "K", "suit": "SPADES"}, {"rank": "Q", "suit": "CLUBS"}]}"""
+
+    def test_board_tuple_encoding(self, json_encoder):
+        board = tuple([Card("Ad"), Card("Ks"), Card("Qc"), Card("Jh"), Card("Ts")])
+        data={}
+        data['board'] = list(board)
+        assert json_encoder.encode(data) == """{"board": [{"rank": "A", "suit": "DIAMONDS"}, {"rank": "K", "suit": "SPADES"}, {"rank": "Q", "suit": "CLUBS"}, {"rank": "J", "suit": "HEARTS"}, {"rank": "T", "suit": "SPADES"}]}"""
+
+    def test_actions_encoding(self, json_encoder):
+        # TODO
+        raise
+
+    def test_street_property_encoding(self, json_encoder):
+        # TODO
+        raise
+
+class TestFullPokerstarsHand:
+
+    def test_hand(self, json_encoder):
+        hand_text = stars_hands.HAND12
+
+        hh = PokerStarsHandHistory(hand_text)
+        hh.parse()
+        json = json_encoder.encode(hh)
+        print(json)
+        assert "\"board\": " in json
